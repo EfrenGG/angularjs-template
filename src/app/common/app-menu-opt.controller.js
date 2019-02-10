3440f99f30452f47a539a794ea0ff6415af0408a
Efren Garcia Guarneros <efren.garcia.guarneros@gmail.com>
2019-02-10 01:44:43 -0600

    Se agregaron los iconos default para las opciones del menú.

--- src/app/common/app-menu-opt.controller.js
+++ src/app/common/app-menu-opt.controller.js
@@ -7,8 +7,11 @@ function appMenuOptController() {
     ctrl.$onChanges = function (changes) {
         if (changes.option) {
             ctrl.option = angular.copy(ctrl.option);
+            if (!ctrl.option.iconoCls) {
+                ctrl.option.iconoCls = ctrl.option.bHoja ? 'fas fa-dot-circle' : 'fas fa-folder';
+            }
         }
-    }
+    };
 }
 
 angular
